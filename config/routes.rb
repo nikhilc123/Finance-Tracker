b30c9ea8886ec53df7c642091949a237edad7ec4
Nikhil N. Chikorde <niks.c91@gmail.com>
2017-10-29 13:55:19 -0400

    Created Friendship model for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => 'user#registrations'}
-  resources :user_stocks, except: [:show, :edit, :update]
   root 'welcome#index'
-
-
+  devise_for :users, :controllers => {:registrations => "user/registrations"}
+  resources :user_stocks, except: [:show, :edit, :update]
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friend'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
