a0fab0d3d7b2ef47c6f0054f558bb424b8670156
Nikhil N. Chikorde <niks.c91@gmail.com>
2017-10-29 11:38:55 -0400

    Shows a list of user_stocks on my_portfolio page User can Delete the stock from that list, NOTE: stock gets deleted from UserStock however, stock remains in the list in the stocks table!

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -76,7 +76,7 @@ class UserStocksController < ApplicationController
   def destroy
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to user_stocks_url, notice: 'User stock was successfully destroyed.' }
+      format.html { redirect_to my_portfolio_path, notice: 'Stock was successfully removed from Portfolio!' }
       format.json { head :no_content }
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
   def my_portfolio
+    #user has_many :stocks
+    #user can access every information of stock table
+    @user_stocks = current_user.stocks
+    @user = current_user
   end
 end
