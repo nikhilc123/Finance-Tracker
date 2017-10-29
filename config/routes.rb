a738f3f0bc53ca58bbc0b1009084325f88ac90cd
Nikhil N. Chikorde <niks.c91@gmail.com>
2017-10-29 12:28:18 -0400

    1. Added first name, last name to User sign up form by   adding migration 2. Created User: registrations controller and added method to sanitize the first name and last name. 3. Updated views in devise -> registration -> new and edit to include first name and last name 4. configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, except: [:show, :edit, :update]
-  resources :user_stocks
+  devise_for :users, :controllers => {:registrations => 'user#registrations'}
+  resources :user_stocks, except: [:show, :edit, :update]
   root 'welcome#index'
 
 
